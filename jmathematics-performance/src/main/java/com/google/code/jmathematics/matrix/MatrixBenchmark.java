e129c1371e5cf6e2f772f3a22c65a0e40da4f51b
Phillip Lincoln Henry <phenry@Phillips-MacBook-Air.local>
2013-05-25 14:45:10 +0100

    Name changes.

--- jmathematics-performance/src/main/java/com/google/code/jmathematics/matrix/MatrixBenchmark.java
+++ jmathematics-performance/src/main/java/com/google/code/jmathematics/matrix/MatrixBenchmark.java
@@ -1,15 +1,15 @@
 package com.google.code.jmathematics.matrix;
 
 import com.google.caliper.SimpleBenchmark;
-import com.google.code.jmathematics.matrix.doubles.Double2DMatrix;
-import com.google.code.jmathematics.matrix.doubles.Mutable2DDoubleMatrix;
-import com.google.code.jmathematics.matrix.integer.Int2DMatrix;
-import com.google.code.jmathematics.matrix.integer.Mutable2DIntMatrix;
-import com.google.code.jmathematics.matrix.longs.Long2DMatrix;
-import com.google.code.jmathematics.matrix.longs.Mutable2DLongMatrix;
-import com.google.code.jmathematics.matrix.number.BigDecimalMutable2DMatrix;
-import com.google.code.jmathematics.matrix.number.IntegerMutable2DMatrix;
-import com.google.code.jmathematics.matrix.number.MutableNumber2DMatrix;
+import com.google.code.jmathematics.matrix.doubles.DoubleMatrix;
+import com.google.code.jmathematics.matrix.doubles.MutableDoubleMatrix;
+import com.google.code.jmathematics.matrix.integer.IntMatrix;
+import com.google.code.jmathematics.matrix.integer.MutableIntMatrix;
+import com.google.code.jmathematics.matrix.longs.LongMatrix;
+import com.google.code.jmathematics.matrix.longs.MutableLongMatrix;
+import com.google.code.jmathematics.matrix.number.BigDecimalMutableMatrix;
+import com.google.code.jmathematics.matrix.number.IntegerMutableMatrix;
+import com.google.code.jmathematics.matrix.number.MutableNumberMatrix;
 
 import java.math.BigDecimal;
 
@@ -46,18 +46,18 @@ import java.math.BigDecimal;
  */
 public class MatrixBenchmark extends SimpleBenchmark {
 
-    private final Mutable2DDoubleMatrix mutable2DDoubleMatrix;
-    private final Mutable2DLongMatrix mutable2DLongMatrix;
-    private final Mutable2DIntMatrix mutable2DIntMatrix;
-    private final IntegerMutable2DMatrix integerMutable2DMatrix;
-    private final BigDecimalMutable2DMatrix bigDecimalMutable2DMatrix;
+    private final MutableDoubleMatrix mutable2DDoubleMatrix;
+    private final MutableLongMatrix mutable2DLongMatrix;
+    private final MutableIntMatrix mutable2DIntMatrix;
+    private final IntegerMutableMatrix integerMutable2DMatrix;
+    private final BigDecimalMutableMatrix bigDecimalMutable2DMatrix;
 
     public MatrixBenchmark() {
-        mutable2DDoubleMatrix = new Mutable2DDoubleMatrix(100, 100);
-        mutable2DLongMatrix = new Mutable2DLongMatrix(100, 100);
-        mutable2DIntMatrix = new Mutable2DIntMatrix(100, 100);
-        integerMutable2DMatrix = new IntegerMutable2DMatrix(100, 100);
-        bigDecimalMutable2DMatrix = new BigDecimalMutable2DMatrix(100, 100);
+        mutable2DDoubleMatrix = new MutableDoubleMatrix(100, 100);
+        mutable2DLongMatrix = new MutableLongMatrix(100, 100);
+        mutable2DIntMatrix = new MutableIntMatrix(100, 100);
+        integerMutable2DMatrix = new IntegerMutableMatrix(100, 100);
+        bigDecimalMutable2DMatrix = new BigDecimalMutableMatrix(100, 100);
         populate(mutable2DDoubleMatrix);
         populate(mutable2DLongMatrix);
         populate(mutable2DIntMatrix);
@@ -65,15 +65,15 @@ public class MatrixBenchmark extends SimpleBenchmark {
         populate(bigDecimalMutable2DMatrix);
     }
 
-    private void populate(BigDecimalMutable2DMatrix bigDecimalMatrix) {
+    private void populate(BigDecimalMutableMatrix bigDecimalMatrix) {
         for (int i = 0 ; i < bigDecimalMatrix.getHeight() ; i++) {
             for (int j = 0 ; j < bigDecimalMatrix.getWidth() ; j++) {
-                bigDecimalMatrix = (BigDecimalMutable2DMatrix) bigDecimalMatrix.set(j, i, new BigDecimal(i * j));
+                bigDecimalMatrix = (BigDecimalMutableMatrix) bigDecimalMatrix.set(j, i, new BigDecimal(i * j));
             }
         }
     }
 
-    private void populate(Double2DMatrix doubleMatrix) {
+    private void populate(DoubleMatrix doubleMatrix) {
         for (int i = 0 ; i < doubleMatrix.getHeight() ; i++) {
             for (int j = 0 ; j < doubleMatrix.getWidth() ; j++) {
                 doubleMatrix = doubleMatrix.set(j, i, j * i * 1.1d);
@@ -81,7 +81,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
         }
     }
     
-    private void populate(Long2DMatrix longMatrix) {
+    private void populate(LongMatrix longMatrix) {
         for (int i = 0 ; i < longMatrix.getHeight() ; i++) {
             for (int j = 0 ; j < longMatrix.getWidth() ; j++) {
                 longMatrix = longMatrix.set(j, i, j * i);
@@ -89,7 +89,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
         }
     }
     
-    private void populate(Int2DMatrix longMatrix) {
+    private void populate(IntMatrix longMatrix) {
         for (int i = 0 ; i < longMatrix.getHeight() ; i++) {
             for (int j = 0 ; j < longMatrix.getWidth() ; j++) {
                 longMatrix = longMatrix.set(j, i, j * i);
@@ -97,10 +97,10 @@ public class MatrixBenchmark extends SimpleBenchmark {
         }
     }
     
-    private void populate(IntegerMutable2DMatrix longMatrix) {
+    private void populate(IntegerMutableMatrix longMatrix) {
         for (int i = 0 ; i < longMatrix.getHeight() ; i++) {
             for (int j = 0 ; j < longMatrix.getWidth() ; j++) {
-                longMatrix = (IntegerMutable2DMatrix) longMatrix.set(j, i, j * i);
+                longMatrix = (IntegerMutableMatrix) longMatrix.set(j, i, j * i);
             }
         }
     }
@@ -110,7 +110,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
      * 50% Scenario{vm=java, trial=0, benchmark=Mutable2DLongMatrixCross} 1402965.83 ns; ?=12970.73 ns @ 6 trials
      */
     public int timeMutable2DLongMatrixCross(int reps) {
-        Mutable2DLongMatrix accumulator = null;
+        MutableLongMatrix accumulator = null;
         for (int i = 0 ; i < reps ; i++) {
             accumulator = mutable2DLongMatrix.cross(mutable2DLongMatrix);
         }
@@ -123,7 +123,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
      *  0% Scenario{vm=java, trial=0, benchmark=Mutable2DDoubleMatrixCross} 1220446.18 ns; ?=53137.85 ns @ 10 trials
      */
     public int timeMutable2DDoubleMatrixCross(int reps) {
-        Mutable2DDoubleMatrix accumulator = null;
+        MutableDoubleMatrix accumulator = null;
         for (int i = 0 ; i < reps ; i++) {
             accumulator = mutable2DDoubleMatrix.cross(mutable2DDoubleMatrix);
         }
@@ -135,7 +135,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
      * 67% Scenario{vm=java, trial=0, benchmark=Mutable2DIntMatrixCross} 1660000.58 ns; ?=194289.51 ns @ 10 trials
      */
     public int timeMutable2DIntMatrixCross(int reps) {
-        Mutable2DIntMatrix accumulator = null;
+        MutableIntMatrix accumulator = null;
         for (int i = 0 ; i < reps ; i++) {
             accumulator = mutable2DIntMatrix.cross(mutable2DIntMatrix);
         }
@@ -147,7 +147,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
      * 75% Scenario{vm=java, trial=0, benchmark=Mutable2DNumberIntMatrixCross} 21126510.42 ns; ?=1989877.40 ns @ 10 trials
      */
     public int timeMutable2DNumberIntMatrixCross(int reps) {
-        MutableNumber2DMatrix accumulator = null;
+        MutableNumberMatrix accumulator = null;
         for (int i = 0 ; i < reps ; i++) {
             accumulator = integerMutable2DMatrix.cross(integerMutable2DMatrix);
         }
@@ -159,7 +159,7 @@ public class MatrixBenchmark extends SimpleBenchmark {
      * 80% Scenario{vm=java, trial=0, benchmark=Mutable2DNumberBigDecimalMatrixCross} 58411000.00 ns; ?=3927557.14 ns @ 10 trials
      */
     public int timeMutable2DNumberBigDecimalMatrixCross(int reps) {
-        MutableNumber2DMatrix accumulator = null;
+        MutableNumberMatrix accumulator = null;
         for (int i = 0 ; i < reps ; i++) {
             accumulator = bigDecimalMutable2DMatrix.cross(bigDecimalMutable2DMatrix);
         }
